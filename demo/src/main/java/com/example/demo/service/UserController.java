214628a3b776c00802c4ebc4377c524bc02f226e
Kernius Brazauskas <brazauskaskernius@gmail.com>
2021-11-01 22:11:00 +0200

    Added model repo and service

---
+++ demo/src/main/java/com/example/demo/service/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // standard constructors
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
